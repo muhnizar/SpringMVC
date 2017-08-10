884f5fbf484f0700c02ffbf88eff7b8c88edb9a7
muhammad.nizar01 <muhammad.nizar01@homecredit.co.id>
2017-08-10 12:57:49 +0700

    CAUTION , THIS IS IMPORTANT : Remove the @repository and add @ transctional to BaseDAO

--- spring-model/src/main/java/com/models/dao/impl/UsersImpl.java
+++ spring-model/src/main/java/com/models/dao/impl/UsersImpl.java
@@ -1,42 +1,51 @@
 package com.models.dao.impl;
 
+import com.models.dao.BaseDAO;
 import com.models.dao.UsersDao;
 import com.models.model.Users;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
+public class UsersImpl extends BaseDAO implements UsersDao {
 
+//    @Autowired
+//    SessionFactory session;
+//
+//    public boolean saveOrUpdate(Users users) {
+//        session.getCurrentSession().saveOrUpdate(users);
+//        return true;
+//    }
+//
+//    public List<Users> list() {
+//        return session.getCurrentSession().createQuery("from Users").list();
+//    }
+//
+//    public boolean delete(Users users) {
+//        try {
+//            session.getCurrentSession().delete(users);
+//        } catch (Exception ex) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
 
-@Repository
-@Transactional
-public class UsersImpl implements UsersDao {
+    public boolean saveOrUpdate(Users users) {
+        return false;
+    }
 
-	@Autowired
-	SessionFactory session;
-	
-	public boolean saveOrUpdate(Users users) {
-		// TODO Auto-generated method stub
-		session.getCurrentSession().saveOrUpdate(users);
-		return true;
-	}
+    public List<Users> list() {
+        return null;
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<Users> list() {
-		return session.getCurrentSession().createQuery("from Users").list();
-	}
+    public boolean delete(Users users) {
+        return false;
+    }
 
-	public boolean delete(Users users) {
-		try{
-			session.getCurrentSession().delete(users);
-		}catch(Exception ex){
-			return false;
-		}
-		
-		return true;
-	}
-	
-	
+    public Users getUserByUsername(String username) {
+        Criteria criteria = getCurrentSession().createCriteria(Users.class);
+        criteria.add(Restrictions.eq("user_name",username));
+        return (Users) criteria.uniqueResult();
+    }
 }
